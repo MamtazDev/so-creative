aff0ceb20120ba6fe7b32964621079de84870f8d
muhammadashiqurrahman <getintouch.ashiq@gmail.com>
2024-02-29 15:32:08 +0600

    ::--update

--- src/Shared/UserPanel/VideoCard.jsx
+++ src/Shared/UserPanel/VideoCard.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 const VideoCard = ({ status, name }) => {
   return (
     <div className="relative">
--- src/components/UserPanel/AllVideos/AllVideo.jsx
+++ src/components/UserPanel/AllVideos/AllVideo.jsx
@@ -3,11 +3,13 @@ import VideoCard from "../../../Shared/UserPanel/VideoCard";
 
 const AllVideo = ({ filteredVideos }) => {
   return (
-    <div className="grid grid-cols-5 gap-6">
-      {filteredVideos.map((data, index) => (
-        <VideoCard key={index} name={data.name} status={data.status} />
-      ))}
-    </div>
+    <>
+      <div className="grid grid-cols-5 gap-6">
+        {filteredVideos?.map((data, index) => (
+          <VideoCard key={index} name={data.name} status={data.status} />
+        ))}
+      </div>
+    </>
   );
 };
 
