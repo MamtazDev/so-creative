fd1488531cc4ba3c92538ca390856b4c02ae2d49
nipaaa <akternipa941@gmail.com>
2024-03-02 14:59:26 +0600

    update

--- src/Shared/UserPanel/BrandCard.jsx
+++ src/Shared/UserPanel/BrandCard.jsx
@@ -2,13 +2,25 @@ import { Plus } from "@phosphor-icons/react";
 import close from "../../assets/close.png";
 
 const BrandCard = ({ brand, inputRef }) => {
+  const fileName = brand.name;
+  const fileNameWithoutExtension = fileName.substring(
+    0,
+    fileName.lastIndexOf(".")
+  );
+  const fileExtension = fileName.substring(fileName.lastIndexOf(".") + 1);
   return (
     <div className="grid grid-cols-5 gap-6">
       <div className="border rounded-xl relative">
         <img className="absolute top-1 right-1" src={close} alt="" />
         <div className="h-[90px] bg-indigo-100 rounded-t-xl"></div>
         <div className="py-2 px-3">
-          <p className="text-sm font-semibold mb-1">{brand.name}</p>
+          <p className="text-sm font-semibold mb-1 ">
+            {" "}
+            {fileNameWithoutExtension.length > 10
+              ? `${fileNameWithoutExtension.substring(0, 10)}`
+              : fileNameWithoutExtension}
+            .{fileExtension}
+          </p>
           <p className="text-xs font-normal">{brand.size / 1000}KB</p>
         </div>
       </div>
