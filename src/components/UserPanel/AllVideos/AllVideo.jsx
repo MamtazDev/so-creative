55e9569ac5ba41f8ffbe06948418f04dd1231ad4
nipaaa <akternipa941@gmail.com>
2024-03-05 15:16:14 +0600

    loading add

--- src/components/UserPanel/AllVideos/AllVideo.jsx
+++ src/components/UserPanel/AllVideos/AllVideo.jsx
@@ -3,17 +3,15 @@ import VideoCard from "../../../Shared/UserPanel/VideoCard";
 
 const AllVideo = ({ filteredVideos }) => {
   return (
-    <>
-      <div className="grid grid-cols-5 gap-6">
-        {filteredVideos.length > 0 ? (
-          filteredVideos?.map((data, index) => (
-            <VideoCard key={index} name={data.name} status={data.status} />
-          ))
-        ) : (
-          <p>There is no data</p>
-        )}
-      </div>
-    </>
+    <div className="grid grid-cols-5 gap-6">
+      {filteredVideos.length > 0 ? (
+        filteredVideos?.map((data, index) => (
+          <VideoCard key={index} name={data.name} status={data.status} />
+        ))
+      ) : (
+        <p>There is no data</p>
+      )}
+    </div>
   );
 };
 
--- src/pages/UserPanel/MediaStorage.jsx
+++ src/pages/UserPanel/MediaStorage.jsx
@@ -2,19 +2,26 @@ import { useState } from "react";
 import AllFiles from "../../components/UserPanel/MediaStorage/AllFiles";
 import MediaHeader from "../../components/UserPanel/MediaStorage/MediaHeader";
 import MediaTable from "../../components/UserPanel/MediaStorage/MediaTable";
+import useLoading from "../../hooks/useLoading";
+import Loading from "../../Shared/Loading";
 
 const MediaStorage = () => {
   const [selectedComponent, setSelectedComponent] = useState("folder");
+  const { isLoading } = useLoading();
   const handleComponentChange = (filter) => {
     setSelectedComponent(filter);
   };
   return (
-    <div>
+    <div className="h-full">
       <MediaHeader
         selectedComponent={selectedComponent}
         handleComponentChange={handleComponentChange}
       />
-      {selectedComponent === "folder" ? <AllFiles /> : <MediaTable />}
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>{selectedComponent === "folder" ? <AllFiles /> : <MediaTable />}</>
+      )}
     </div>
   );
 };
