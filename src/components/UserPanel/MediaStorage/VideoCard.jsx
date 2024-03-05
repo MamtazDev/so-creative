0af9ab1ceac79d3fb38f81e759643bf5e2ba8da0
nipaaa <akternipa941@gmail.com>
2024-03-05 10:00:17 +0600

    update

--- src/components/UserPanel/MediaStorage/VideoCard.jsx
+++ src/components/UserPanel/MediaStorage/VideoCard.jsx
@@ -1,8 +1,12 @@
+import { DotsThreeOutline } from "@phosphor-icons/react";
 import mp4 from "../../../assets/mp4.svg";
 
 const VideoCard = () => {
   return (
-    <div className="text-center">
+    <div className="text-center relative">
+      <button className="absolute -top-6 -right-6">
+        <DotsThreeOutline className="text-slate-500" size={20} weight="fill" />
+      </button>
       <img className="m-auto mb-2" src={mp4} alt="" />
       <p className="text-base font-bold mb-2">filename.mp4</p>
       <p className="text-slate-400 text-xs">1.26 GB</p>
