baa1644fa669546675489fd51c88997cb8bde0eb
nipaaa <akternipa941@gmail.com>
2024-03-06 13:02:21 +0600

    update

--- src/components/UserPanel/CreateProject/UploadFile.jsx
+++ src/components/UserPanel/CreateProject/UploadFile.jsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from "react";
+import { useState } from "react";
 import { useDropzone } from "react-dropzone";
 import mp4 from "../../../assets/mp4.svg";
 import SelectedVideo from "./SelectedVideo";
@@ -6,16 +6,32 @@ import SelectedVideo from "./SelectedVideo";
 const UploadFile = () => {
   const [selectedVideos, setSelectedVideos] = useState([]);
   const [isDragActive, setIsDragActive] = useState(false);
+  const [isUploading, setIsUploading] = useState(false);
+  const [uploadProgress, setUploadProgress] = useState(0);
 
   const { getRootProps, getInputProps } = useDropzone({
     accept: {
       "video/mp4": [".mp4"],
     },
     onDrop: (acceptedFiles) => {
-      if (acceptedFiles && acceptedFiles.length > 0) {
-        setSelectedVideos([...selectedVideos, ...acceptedFiles]);
-      }
-      setIsDragActive(false);
+      setIsUploading(true);
+      let progress = 0;
+
+      const upload = () => {
+        if (progress < 100) {
+          setTimeout(() => {
+            progress += Math.random() * 10;
+            setUploadProgress(progress);
+            upload();
+          }, 500); // Simulate upload progress every 0.5 seconds
+        } else {
+          setIsUploading(false);
+          setUploadProgress(0);
+          setSelectedVideos([...selectedVideos, ...acceptedFiles]);
+        }
+      };
+
+      upload();
     },
     onDragEnter: () => {
       setIsDragActive(true);
@@ -32,6 +48,17 @@ const UploadFile = () => {
 
   return (
     <div className="border border-dashed rounded-xl text-center">
+      {isUploading && (
+        <div>
+          <div className="bg-indigo-50 h-2 rounded-md">
+            <div
+              style={{ width: `${uploadProgress}%` }}
+              className="bg-indigo-600 rounded-md h-2"
+            ></div>
+          </div>
+          <p>{uploadProgress}%</p>
+        </div>
+      )}
       <div {...getRootProps({ style: dashedBoxStyle })} className="p-12">
         <input {...getInputProps()} />
         <img className="m-auto mb-5" src={mp4} alt="" />
