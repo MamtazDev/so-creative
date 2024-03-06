35cd82d9399dcf80a1e165bf858ea1765812881c
nipaaa <akternipa941@gmail.com>
2024-03-06 15:32:46 +0600

    update meia table

--- src/components/UserPanel/MediaStorage/MediaTable.jsx
+++ src/components/UserPanel/MediaStorage/MediaTable.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import mp4 from "../../../assets/mp4.svg";
 import folder from "../../../assets/folder.svg";
-import { DotsThreeOutline } from "@phosphor-icons/react";
+import {
+  DotsThreeOutline,
+  DownloadSimple,
+  NotePencil,
+  Trash,
+} from "@phosphor-icons/react";
 import { mediaStorage } from "../../../utils/data";
 
 const MediaTable = () => {
@@ -38,11 +43,32 @@ const MediaTable = () => {
                 <td>1.26 GB</td>
                 <td>1 day ago</td>
                 <td>
-                  <DotsThreeOutline
-                    className="text-slate-500"
-                    size={20}
-                    weight="fill"
-                  />
+                  {data.folder ? (
+                    <button>
+                      <NotePencil
+                        className="text-indigo-600"
+                        size={20}
+                        weight="fill"
+                      />
+                    </button>
+                  ) : (
+                    <div className="flex  items-center gap-2">
+                      <button>
+                        <Trash
+                          className="text-red-600"
+                          size={20}
+                          weight="fill"
+                        />
+                      </button>
+                      <button>
+                        <DownloadSimple
+                          className="text-indigo-600"
+                          size={20}
+                          weight="fill"
+                        />
+                      </button>
+                    </div>
+                  )}
                 </td>
               </tr>
             ))
