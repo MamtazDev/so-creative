8f10cdf86be9cd31556a4e2272321df33c7a120e
mohi14 <mohiuddinkhan1409@gmail.com>
2024-03-06 10:28:27 +0600

    redux setup

---
+++ src/features/videos/videoApi.js
@@ -0,0 +1,42 @@
+import { apiSlice } from "../api/apiSlice";
+
+export const videoApi = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    createFolder: builder.mutation({
+      query: (data) => ({
+        url: "/api/v1/medias/createFolder",
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["Drives"],
+    }),
+    addFile: builder.mutation({
+      query: (data) => ({
+        url: "/api/v1/medias/addFile",
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["Videos", "Drives", "Folders"],
+    }),
+    getUserDrive: builder.query({
+      query: () => "/api/v1/medias/getUserDrive",
+      providesTags: ["Drives"],
+    }),
+    getFolderFile: builder.query({
+      query: (id) => `/api/v1/medias/getFolderFile/${id}`,
+      providesTags: ["Folders"],
+    }),
+    getUserAllFiles: builder.query({
+      query: () => "/api/v1/medias/getUserAllFiles",
+      providesTags: ["Videos"],
+    }),
+  }),
+});
+
+export const {
+  useCreateFolderMutation,
+  useAddFileMutation,
+  useGetUserDriveQuery,
+  useGetFolderFileQuery,
+  useGetUserAllFilesQuery,
+} = videoApi;
--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,6 +1,56 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useLoginMutation } from "../../features/auth/authApi";
+import Swal from "sweetalert2";
+import { useState } from "react";
 
 const Login = () => {
+  const [login, { isLoading }] = useLoginMutation();
+  const [loging, setLoging] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoging(true);
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    const data = {
+      email,
+      password,
+    };
+
+    try {
+      const res = await login(data);
+
+      if (res?.error?.error) {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: `${res?.error?.error}`,
+        });
+      }
+      if (res?.error?.data?.message) {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: `${res?.error?.data?.message}`,
+        });
+      }
+      if (res?.data?.success) {
+        navigate("/user");
+      }
+    } catch (error) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: `${error?.message}`,
+      });
+    } finally {
+      setLoging(false);
+    }
+  };
   return (
     <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
       <div class="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -15,7 +65,7 @@ const Login = () => {
       </div>
 
       <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form class="space-y-6" action="#" method="POST">
+        <form class="space-y-6" onSubmit={handleSubmit}>
           <div>
             <label
               for="email"
@@ -66,6 +116,7 @@ const Login = () => {
 
           <div>
             <button
+              disabled={loging || isLoading}
               type="submit"
               class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
