5e09f24afd5f93b90c76bdc8c2ea8d84e712356d
nipaaa <akternipa941@gmail.com>
2024-03-06 12:36:25 +0600

    update

--- src/components/UserPanel/CreateProject/UploadFile.jsx
+++ src/components/UserPanel/CreateProject/UploadFile.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { useDropzone } from "react-dropzone";
 import mp4 from "../../../assets/mp4.svg";
 import SelectedVideo from "./SelectedVideo";
 
 const UploadFile = () => {
   const [selectedVideos, setSelectedVideos] = useState([]);
+  const [isDragActive, setIsDragActive] = useState(false);
 
   const { getRootProps, getInputProps } = useDropzone({
     accept: {
@@ -14,13 +15,24 @@ const UploadFile = () => {
       if (acceptedFiles && acceptedFiles.length > 0) {
         setSelectedVideos([...selectedVideos, ...acceptedFiles]);
       }
+      setIsDragActive(false);
+    },
+    onDragEnter: () => {
+      setIsDragActive(true);
+    },
+    onDragLeave: () => {
+      setIsDragActive(false);
     },
   });
-  console.log(selectedVideos, "ooo");
+
+  const dashedBoxStyle = {
+    border: isDragActive ? "1px dashed #4F46E5 " : "1px dashed #E2E8F0",
+    background: isDragActive ? "#EEF2FF" : "",
+  };
 
   return (
     <div className="border border-dashed rounded-xl text-center">
-      <div {...getRootProps()} className="p-12">
+      <div {...getRootProps({ style: dashedBoxStyle })} className="p-12">
         <input {...getInputProps()} />
         <img className="m-auto mb-5" src={mp4} alt="" />
         <p className="text-lg font-semibold mb-1">Upload a File or</p>
