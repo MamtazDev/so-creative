82ab3a80d057808af716ece2447e6d0b99af8a03
nipaaa <akternipa941@gmail.com>
2024-03-08 10:59:40 +0600

    aspect ratio active

--- src/components/UserPanel/CreateProject/AspectRatio.jsx
+++ src/components/UserPanel/CreateProject/AspectRatio.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import FromLabel from "./FromLabel";
 import { Link } from "react-router-dom";
 import { ArrowUpRight } from "@phosphor-icons/react";
 import { aspectRatio } from "../../../utils/data";
 
 const AspectRatio = () => {
+  const [selectedRatio, setSelectedRatio] = useState(null);
+
+  const handleInputChange = (name) => {
+    setSelectedRatio(name);
+  };
+
+  const isChecked = (name) => {
+    return selectedRatio === name;
+  };
   return (
     <div className="border rounded-xl p-6">
       <div className="flex gap-4 justify-between items-start mb-6">
@@ -22,28 +31,49 @@ const AspectRatio = () => {
       <div className="flex items-center gap-4">
         {aspectRatio.length > 0 &&
           aspectRatio.map((data, index) => (
-            <div
-              className="border text-center rounded-xl p-2 w-[120px] h-[124px] text-sm font-semibold"
+            <label
+              htmlFor={data.name}
+              className={`${
+                isChecked(data.name) ? "border-indigo-600" : "border-slate-200"
+              } border text-center rounded-xl p-2 w-[120px] h-[124px] text-sm font-semibold`}
               key={index}
             >
               <div className="text-end">
-                <input type="radio" />
+                <input
+                  id={data.name}
+                  type="radio"
+                  name="ratio"
+                  value={data.name}
+                  onChange={() => handleInputChange(data.name)}
+                />
               </div>
+
               <img className="m-auto" src={data.pic} alt="" />
               <p className="mt-2">{data.ratio}</p>
               <p>{data.name}</p>
-            </div>
+            </label>
           ))}
-        <div className="border text-center rounded-xl p-2 w-[120px] h-[124px] text-sm font-semibold">
+        <label
+          htmlFor="custom"
+          className={`${
+            isChecked("custom") ? "border-indigo-600" : "border-slate-200"
+          } border text-center rounded-xl p-2 w-[120px] h-[124px] text-sm font-semibold`}
+        >
           <div className="text-end">
-            <input type="radio" />
+            <input
+              type="radio"
+              id="custom"
+              name="ratio"
+              value="custom"
+              onChange={() => handleInputChange("custom")}
+            />
           </div>
 
           <p className="mb-5">Custom</p>
           <input className="brief_input" type="text" placeholder="Type Here" />
-        </div>
+        </label>
       </div>
-      <button className="py-3 px-6 bg-indigo-600 rounded-full text-white text-base font-semibold ">
+      <button className="mt-6 py-3 px-6 bg-indigo-600 rounded-full text-white text-base font-semibold ">
         Continue
       </button>
     </div>
