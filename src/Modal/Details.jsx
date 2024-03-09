3ed50cfa57074cedac3c14277b1c75c2b2c01636
nipaaa <akternipa941@gmail.com>
2024-03-09 12:49:54 +0600

    select draft add

---
+++ src/Modal/Details.jsx
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+import {
+  CaretDown,
+  CaretLeft,
+  CaretUp,
+  FileText,
+  PuzzlePiece,
+} from "@phosphor-icons/react";
+import cc from "../assets/cc.svg";
+import voice from "../assets/voice.svg";
+
+const Details = () => {
+  const [showFullText, setShowFullText] = useState(false);
+  const maxWords = 110;
+  const text =
+    " This project aims to create a video series focusing on impactful sales strategies for businesses of all sizes. The series will leverage your on-demand video editing service to showcase its capabilities and attract potential clients, while providing valuable and actionable sales advice. <br /> <br /> To achieve your project goals, a multifaceted approach blending  content marketing, social media engagement, and strategic partnerships is essential. Crafting informative blog posts, videos, and social media content showcasing the benefits of your on-demand video editing service will help increase brand awareness and position your company as thought leaders in the sales and marketing realm. Implementing targeted advertising campaigns to reach potential clients and offering valuable resources such as eBooks or webinars on leveraging video editing for sales enhancement will generate leads and conversions. Collaborating with industry influencers and participating in relevant events or webinars will further solidify your company's reputation and expand your reach within the sales and marketing community.";
+
+  const truncateText = (text, maxWords) => {
+    const words = text.split(" ");
+    if (words.length > maxWords) {
+      return words.slice(0, maxWords).join(" ") + "...";
+    }
+    return text;
+  };
+  return (
+    <div>
+      <div className="flex items-start gap-4 justify-between mb-10">
+        <div>
+          <div className="flex gap-3 items-center">
+            <p className="text-lg font-semibold">
+              Sales Strategies Video Series
+            </p>
+            <button className="px-1.5 py-0.5 bg-slate-900 text-white rounded-md">
+              Draft
+            </button>
+          </div>
+          <p className="text-slate-600 text-sm font-normal">
+            Created September 1, 2023
+          </p>
+        </div>
+        <button className="flex items-center gap-2 px-6 py-3 text-base font-semibold text-indigo-600 border rounded-full border-indigo-600">
+          <CaretLeft size={24} /> Go Back
+        </button>
+      </div>
+      <div>
+        <div className="flex items-center gap-2 mb-4">
+          <FileText className="text-indigo-600 " size={24} />
+          <p className="text-lg font-semibold">Project Brief</p>
+        </div>
+
+        <div className="border rounded-3xl mb-10">
+          <div className="p-6 border-b">
+            <p
+              className="text-slate-800 font-normal text-sm"
+              dangerouslySetInnerHTML={{
+                __html: showFullText ? text : truncateText(text, maxWords),
+              }}
+            />
+          </div>
+          <button
+            className="flex gap-2 items-center justify-center w-full text-sm p-4 font-semibold text-indigo-600"
+            onClick={() => setShowFullText(!showFullText)}
+          >
+            {showFullText ? (
+              <>
+                Show Less <CaretUp size={20} />
+              </>
+            ) : (
+              <>
+                Show More <CaretDown size={20} />
+              </>
+            )}
+          </button>
+        </div>
+        <div className="flex items-center gap-2 mb-4">
+          <PuzzlePiece className="text-indigo-600 " size={24} />
+          <p className="text-lg font-semibold">Add-Ons</p>
+        </div>
+
+        <div className="border rounded-3xl mb-10">
+          <div className="p-4 border-b flex items-center gap-2 justify-between">
+            <div className="flex items-center gap-4">
+              <img src={cc} alt="" />
+              <div>
+                <p className="text-slate-800 mb-1 text-base font-semibold">
+                  Subtitles and SRT Files
+                </p>
+                <p className="text-slate-500 font-normal text-xs">
+                  Spoken English videos only. Allow up to 24-48 hours.
+                </p>
+              </div>
+            </div>
+            <p>0.5 credits</p>
+          </div>
+        </div>
+      </div>
+      <button className="primary_btn w-full">Select Draft</button>
+    </div>
+  );
+};
+
+export default Details;
--- src/Modal/SelectDraft.jsx
+++ src/Modal/SelectDraft.jsx
@@ -1,31 +1,26 @@
-import React, { useRef } from "react";
-import close from "../assets/close.svg";
+import { useRef, useState } from "react";
 import useOutsideClick from "../hooks/useOutsideClick";
-import { MagnifyingGlass } from "@phosphor-icons/react";
+import Draft from "./Draft";
+import Details from "./Details";
 
 const SelectDraft = ({ setShowDraftModal }) => {
   const draftRef = useRef();
+  const [step, setStep] = useState(1);
   useOutsideClick(draftRef, () => setShowDraftModal(false));
+
   return (
     <div className="fixed left-0 top-0 z-50 h-screen w-full bg-[#00000080] backdrop-blur-xl flex items-center justify-center">
-      <div className="max-w-[896px] max-h-[90vh] overflow-y-auto no_scrollbar w-full bg-white text-black p-10 rounded-2xl relative">
-        <div className="flex items-center gap-4 justify-between mb-6">
-          <p className="text-xl font-bold">Select From Draft</p>
+      <div
+        ref={draftRef}
+        className="max-w-[896px] max-h-[90vh] overflow-y-auto no_scrollbar w-full bg-white text-black p-10 rounded-2xl relative"
+      >
+        {step === 1 && (
+          <Draft setStep={setStep} setShowDraftModal={setShowDraftModal} />
+        )}
 
-          <button onClick={() => setShowDraftModal(false)}>
-            <img src={close} alt="" />
-          </button>
-        </div>
-        <div className="flex items-center gap-5 rounded-full bg-slate-100 p-3 w-full mb-10">
-          <MagnifyingGlass size={16} />
-          <input
-            className="w-full bg-transparent"
-            type="search"
-            placeholder="Search projects"
-          />
-        </div>
-
-        <button className="primary_btn w-full">Continue</button>
+        {step === 2 && (
+          <Details setStep={setStep} setShowDraftModal={setShowDraftModal} />
+        )}
       </div>
     </div>
   );
