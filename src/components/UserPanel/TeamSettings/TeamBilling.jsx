a14b5c246e24778f6b4df56e04e0d16daf0082dd
nipaaa <akternipa941@gmail.com>
2024-03-11 15:09:42 +0600

    update

--- src/components/UserPanel/TeamSettings/TeamBilling.jsx
+++ src/components/UserPanel/TeamSettings/TeamBilling.jsx
@@ -1,4 +1,5 @@
 import plan from "../../../assets/enterprise-plan.svg";
+import PaymentDetails from "./PaymentDetails";
 
 const TeamBilling = () => {
   return (
@@ -27,48 +28,7 @@ const TeamBilling = () => {
           Upgrade
         </button>
       </div>
-
-      <p className="text-base font-semibold">Payment Method</p>
-      <table className="w-full text-slate-700 text-sm font-normal">
-        <tbody>
-          <tr>
-            <td className="border-b">Payment Method</td>
-            <td className="border-b">PayPal</td>
-            <td className="border-b text-end">
-              <button className="font-semibold text-indigo-600 py-4">
-                Edit
-              </button>
-            </td>
-          </tr>
-          <tr>
-            <td className="border-b">Billing Interval</td>
-            <td className="border-b">Annually</td>
-            <td className="border-b text-end">
-              <button className="font-semibold text-indigo-600 py-4">
-                Edit
-              </button>
-            </td>
-          </tr>
-          <tr>
-            <td className="border-b">Name</td>
-            <td className="border-b">James</td>
-            <td className="border-b text-end">
-              <button className="font-semibold text-indigo-600 py-4">
-                Edit
-              </button>
-            </td>
-          </tr>
-          <tr>
-            <td>Email</td>
-            <td>james@example.com</td>
-            <td className="text-end">
-              <button className="font-semibold text-indigo-600 py-4">
-                Edit
-              </button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <PaymentDetails />
     </div>
   );
 };
