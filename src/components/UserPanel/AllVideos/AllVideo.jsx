fa1f4bba8add0dac4657a5b3ea29358d3650e8d0
nipaaa <akternipa941@gmail.com>
2024-03-11 13:15:38 +0600

    invite people add

--- src/components/UserPanel/AllVideos/AllVideo.jsx
+++ src/components/UserPanel/AllVideos/AllVideo.jsx
@@ -5,7 +5,7 @@ import { truncateFilename } from "../../../utils/converter";
 const AllVideo = ({ filteredVideos }) => {
   return (
     <div className="grid grid-cols-5 gap-6">
-      {filteredVideos.length > 0 ? (
+      {filteredVideos?.length > 0 ? (
         filteredVideos?.map((data, index) => (
           <VideoCard
             key={index}
