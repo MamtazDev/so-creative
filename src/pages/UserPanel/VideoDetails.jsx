bedd86788198066ff73d03d643ef9cd4e5a15480
nipaaa <akternipa941@gmail.com>
2024-03-13 11:07:44 +0600

    download verity add

--- src/pages/UserPanel/VideoDetails.jsx
+++ src/pages/UserPanel/VideoDetails.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import DetailsHeader from "../../components/UserPanel/AllVideos/VideoDetails/DetailsHeader";
+import Details from "../../components/UserPanel/AllVideos/VideoDetails/Details";
 
 const VideoDetails = () => {
   return (
     <div>
       <DetailsHeader />
+      <div className="grid grid-cols-3 gap-[30px]">
+        <div className="col-span-2">
+          <Details />
+        </div>
+      </div>
     </div>
   );
 };
