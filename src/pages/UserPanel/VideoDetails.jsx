332822b264782a2a2a4b59bbb846fdd6e784a6d4
nipaaa <akternipa941@gmail.com>
2024-03-13 10:03:19 +0600

    video details header add

---
+++ src/pages/UserPanel/VideoDetails.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DetailsHeader from "../../components/UserPanel/AllVideos/VideoDetails/DetailsHeader";
+
+const VideoDetails = () => {
+  return (
+    <div>
+      <DetailsHeader />
+    </div>
+  );
+};
+
+export default VideoDetails;
