1c986e0175bc6f5789199af9ad05c54e36d48a4d
nipaaa <akternipa941@gmail.com>
2024-03-28 11:09:28 +0600

    chatbot update

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -3,7 +3,6 @@ import Header from "../Shared/UserPanel/Header";
 import Sidebar from "../Shared/UserPanel/Sidebar";
 import ChatBot from "../Shared/UserPanel/ChatBot";
 import { useSelector } from "react-redux";
-import ChatBotV2 from "../Shared/UserPanel/ChatBotV2";
 
 const MainLayout = () => {
   const { user } = useSelector((state) => state.auth);
@@ -15,8 +14,7 @@ const MainLayout = () => {
           <Sidebar user={user} />
           <div className="mr-8 mb-8 ml-8 p-10 bg-white rounded-3xl w-full overflow-y-auto no_scrollbar">
             <Outlet />
-            {/* {user?.role === "USER" && <ChatBot />} */}
-            {user?.role === "USER" && <ChatBotV2 />}
+            {user?.role === "USER" && <ChatBot />}
           </div>
         </div>
       </div>
