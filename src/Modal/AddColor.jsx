c5a95d18a21e6a7f740e2d55df83dd4d6e418125
nipaaa <akternipa941@gmail.com>
2024-04-01 11:58:29 +0600

    update

--- src/Modal/AddColor.jsx
+++ src/Modal/AddColor.jsx
@@ -2,7 +2,12 @@ import React, { useRef, useState } from "react";
 import useOutsideClick from "../hooks/useOutsideClick";
 import close from "../assets/close.svg";
 
-const AddColor = ({ setColorModal, selectedColor, setSelectedColor }) => {
+const AddColor = ({
+  setColorModal,
+  selectedColor,
+  setSelectedColor,
+  handleChange,
+}) => {
   const colorRef = useRef();
 
   const handleColorChange = (e) => {
@@ -13,7 +18,7 @@ const AddColor = ({ setColorModal, selectedColor, setSelectedColor }) => {
     <div className="fixed left-0 top-0 z-[9999] h-screen w-full bg-[#00000080] backdrop-blur-xl flex items-center justify-center">
       <div
         ref={colorRef}
-        className="max-w-[640px] w-full bg-white text-black p-10 rounded-2xl relative"
+        className="max-w-[540px] w-full bg-white text-black p-10 rounded-2xl relative"
       >
         <button
           onClick={() => setColorModal(false)}
@@ -21,27 +26,36 @@ const AddColor = ({ setColorModal, selectedColor, setSelectedColor }) => {
         >
           <img src={close} alt="" />
         </button>
-        <div className="flex justify-between items-center gap-4 mt-4">
-          <div>
-            <label className="block mb-2">Choose color from here</label>
-            <input
-              type="color"
-              value={selectedColor}
-              onChange={handleColorChange}
-              className="rounded-md"
-            />
-          </div>
-          <div>
-            <input
-              type="text"
-              className="border border-gray-300 p-2 rounded-md"
-              value={selectedColor}
-              onChange={handleColorChange}
-            />
-          </div>
+
+        <div className="mb-5">
+          <label className="block mb-2">Choose color from here</label>
+          <input
+            type="color"
+            name="color"
+            value={selectedColor}
+            onChange={handleChange}
+            // onChange={handleColorChange}
+            className="rounded-md w-full"
+          />
         </div>
+        <div>
+          <label className="block mb-2">Type here</label>
+          <input
+            type="text"
+            className="border border-gray-300 p-2 rounded-md w-full"
+            value={selectedColor}
+            onChange={handleColorChange}
+          />
+        </div>
+
         <div className="text-center">
-          <button className="primary_btn mt-5 ">Add</button>
+          <button
+            onClick={() => setColorModal(false)}
+            type="button"
+            className="primary_btn mt-5 "
+          >
+            Add
+          </button>
         </div>
       </div>
     </div>
