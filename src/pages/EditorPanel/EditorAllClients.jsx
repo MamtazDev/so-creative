0b89b9654f0362342ac8222d83f15fb1e2e2e96c
mohi14 <mohiuddinkhan1409@gmail.com>
2024-04-07 13:41:06 +0600

    mohi client update

--- src/pages/EditorPanel/EditorAllClients.jsx
+++ src/pages/EditorPanel/EditorAllClients.jsx
@@ -1,31 +1,55 @@
-import { useState } from 'react';
-import Loading from '../../Shared/Loading';
-import MyWorkTable from '../../components/EditorPanel/EditorDashboard/MyWorkTable';
-import EditroSectionTitle from '../../components/EditorPanel/EditorSectionTitle/EditroSectionTitle';
-import useLoading from '../../hooks/useLoading';
-import { MyWorkTableData } from '../../utils/data';
+import { useState } from "react";
+import Loading from "../../Shared/Loading";
+import MyWorkTable from "../../components/EditorPanel/EditorDashboard/MyWorkTable";
+import EditroSectionTitle from "../../components/EditorPanel/EditorSectionTitle/EditroSectionTitle";
+import useLoading from "../../hooks/useLoading";
+import { MyWorkTableData } from "../../utils/data";
+import { useGetAllProjectsQuery } from "../../features/project/projectApi";
+import { useSelector } from "react-redux";
 
 const EditorAllClients = () => {
-      const { isLoading } = useLoading();
-      const [filter, setFilter] = useState("All");
-      const handleFilterChange = (newFilter) => {
-        setFilter(newFilter);
-      };
+  const [filter, setFilter] = useState("All");
+  const handleFilterChange = (newFilter) => {
+    setFilter(newFilter);
+  };
 
-      const repeatedData = Array.from(
-        { length: 5 },
-        () => MyWorkTableData
-      ).flat();
+  const { user } = useSelector((state) => state.auth);
+  const { data, isLoading } = useGetAllProjectsQuery(`editor=${user?._id}`);
 
-      const filteredData = repeatedData.filter((item) =>
-        filter === "All"
-          ? true
-          : filter === "Active"
-          ? item.status === "In Progress" || item.status === "New Project"
-          : filter === "Completed"
-          ? item.status === "Approved"
-          : true
+  const result =
+    data &&
+    data.reduce((acc, project) => {
+      const lastProject = data.filter(
+        (i) => i?.creator?._id === project?.creator?._id
       );
+      const existingProject = acc.find(
+        (item) => item?.id === project?.creator?._id
+      );
+      if (existingProject) {
+        existingProject.projectCount++;
+      } else {
+        acc.push({
+          name: project.creator.name,
+          id: project?.creator?._id,
+          projectCount: 1,
+          lastProject: lastProject[0],
+          image: project?.creator?.image,
+        });
+      }
+      return acc;
+    }, []);
+
+  console.log(result, "result");
+
+  // const filteredData = repeatedData.filter((item) =>
+  //   filter === "All"
+  //     ? true
+  //     : filter === "Active"
+  //     ? item.status === "In Progress" || item.status === "New Project"
+  //     : filter === "Completed"
+  //     ? item.status === "Approved"
+  //     : true
+  // );
   return (
     <>
       {isLoading ? (
@@ -37,13 +61,11 @@ const EditorAllClients = () => {
             handleFilterChange={handleFilterChange}
           />
 
-          <MyWorkTable
-            filteredData={filteredData}
-          />
+          <MyWorkTable filteredData={result} />
         </>
       )}
     </>
   );
-}
+};
 
-export default EditorAllClients
+export default EditorAllClients;
