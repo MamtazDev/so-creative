1ecd74f3c679eb53f438e2a0e05f7297dabbb79a
MamtazDev <akter.mstmamtaz@gmail.com>
2024-04-23 10:40:05 +0600

    |redeploy the site env

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,7 @@
-export const BASE_API_URL = "http://localhost:8000";
+// export const BASE_API_URL = "http://localhost:8000";
 
-// export const BASE_API_URL = "https://socreative-server.onrender.com";
+export const BASE_API_URL = "https://socreative-server.onrender.com";
+
 export const imageHostKey = "7378254be2fef904c69a0c05769ced22";
 
 
